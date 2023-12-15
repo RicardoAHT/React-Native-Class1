3fe30764554f49dd48f53b83cdb33034d9ee228a
Ricardo <rickxd007@gmail.com>
2023-12-15 18:00:24 -0500

    Utilizo StatusBar de expo para modificar el status bar

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Main from './src/components/Main'
 import { NativeRouter } from "react-router-native"
+import { StatusBar, statusBar } from "expo-status-bar"
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Main/>
-    </NativeRouter>   
+    <>
+      <StatusBar style="light" />
+      <NativeRouter>
+        <Main/>
+      </NativeRouter>   
+    </>
 
   );
 }
